b06b8d270e4b80230585c10ed77d437d26e9d8f8
AlexandreLec <alexandre.lecomte@viacesi.fr>
2017-06-01 17:28:20 +0200

    Phase 3

--- source/jpu2016/dogfight/view/DogfightView.java
+++ source/jpu2016/dogfight/view/DogfightView.java
@@ -1,6 +1,32 @@
 package jpu2016.dogfight.view;
 
-public class DogfightView implements Runnable {
+import java.util.Observable;
+
+import jpu2016.dogfight.controller.IOrderPerformer;
+import jpu2016.dogfight.model.IDogfightModel;
+
+public class DogfightView implements Runnable, IViewSystem {
+
+	private EventPerformer eventPerformer;
+	private GraphicsBuilder graphicsBuilder; 
+	
+	public DogfightView(IOrderPerformer orderPerformer, IDogfightModel dogfightModel, Observable observable){
+		
+		this.eventPerformer = new EventPerformer(orderPerformer);
+		this.graphicsBuilder = new GraphicsBuilder(dogfightModel);
+	}
+
+	@Override
+	public void closeAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String displayMessage(String message) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
 	public void run() {
--- source/jpu2016/dogfight/view/EventPerformer.java
+++ source/jpu2016/dogfight/view/EventPerformer.java
@@ -1,5 +1,27 @@
 package jpu2016.dogfight.view;
 
-public class EventPerformer {
+import java.awt.event.KeyEvent;
 
+import jpu2016.dogfight.controller.IOrderPerformer;
+import jpu2016.dogfight.controller.UserOrder;
+import jpu2016.gameframe.IEventPerformer;
+
+public class EventPerformer implements IEventPerformer{
+	
+	private IOrderPerformer orderPerformer;
+
+	public EventPerformer(IOrderPerformer orderPerformer){
+		this.orderPerformer = orderPerformer;
+	}
+
+	@Override
+	public void eventPerform(KeyEvent keyCode) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/*private UserOrder keyCodeToUserOrder(int keyCode){
+		
+		return new UserOrder(keyCode, keyCode); 
+	}*/
 }
